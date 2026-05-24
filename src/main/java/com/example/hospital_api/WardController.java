f8834621375f5c8bcb5c0d6bd785b7d22b2ca175
Tejas1102 <hadgetejas02@gmail.com>
2026-05-24 16:53:45 +0530

    Fixed deploy render bug

--- src/main/java/com/example/hospital_api/WardController.java
+++ src/main/java/com/example/hospital_api/WardController.java
@@ -1,4 +1,4 @@
-package com.example.hospitalapi;
+package com.example.hospital_api;
 
 import java.util.List;
 import java.util.stream.Collectors;
--- src/main/java/com/example/hospital_api/WardRecord.java
+++ src/main/java/com/example/hospital_api/WardRecord.java
@@ -1,4 +1,4 @@
-package com.example.hospitalapi;
+package com.example.hospital_api;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
